8b96578be19a7d4e60bdb85f69d01852912eb364
Ritchie Fu <fu.ritchie@gmail.com>
2025-11-11 15:24:20 -0800

    WPD3 update: login page trying to use Stytch, pushing so I can work on my desktop

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,3 +1,31 @@
+// Login.vue
+<script setup>
+import { onMounted } from 'vue'
+import { StytchUIClient, Products } from '@stytch/vanilla-js'
+require('dotenv').config();
+
+const publicToken = process.env.STYTCH_PUBLIC_TOKEN
+const stytch = new StytchUIClient(publicToken)
+
+const config = {
+  products: [Products.emailMagicLinks, Products.oauth],
+  oauthOptions: {
+    providers: [{ type: 'google' }],
+  }
+}
+
+
+onMounted(() => {
+  stytch.mountLogin({
+    elementId: '#stytch-sdk',
+    styles,
+    config,
+  })
+})
+
+</script>
 <template>
-    <h1>Login Page</h1>
+  <div class="container">
+    <div id="stytch-sdk"></div>
+  </div>
 </template>
